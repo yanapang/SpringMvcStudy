8c0b2c5c7a222f07e87f5a1cfc5c4ba39428f05d
yanapang <yanahwang@gmail.com>
2023-04-09 13:42:31 +0900

    [Servlet] Response. 3)HTTP API. 객체에 값 setting후 ObjectMapper를 통해 JSON으로 변환해 반환.

---
+++ src/main/java/hello/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "responseJsonServlet", urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type:application/json
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8"); //aplication/json 은 자동으로 utf-8. 추가 안해도 됨.
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //ObjectMapper : Object to JSON
+        String result = objectMapper.writeValueAsString(helloData);
+        response.getWriter().write(result);
+    }
+}
